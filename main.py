c7a3d7bb4aae6401e7ba7f9b4cc16828bf9b6e2b
Will Wagner <wowagner2015@yahoo.com>
2026-01-25 13:12:31 -0800

    all in one pipeline

--- main.py
+++ main.py
@@ -6,12 +6,12 @@ from collections import Counter
 url = input("Enter the URL: ") # Replace with your URL
 html_content = requests.get(url).text
 soup = BeautifulSoup(html_content, "html.parser")
-
 all_text = soup.get_text()
 text = [
     chunk.strip().lower()
     for chunk in re.split(
-        r'[_&\d\n\s[^\w\s](?=[A-Z])]+', all_text
+        r'[\d\n]|[^\w\s]|(?=[A-Z])|\s+',
+        all_text
     )
     if chunk.strip()
 ]
