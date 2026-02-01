5c72cc9a6a271549a01e1654a9fc21233288a26a
Will Wagner <wowagner2015@yahoo.com>
2026-01-31 21:37:21 -0800

    streamlit app

--- main.py
+++ main.py
@@ -2,21 +2,28 @@ import requests
 from bs4 import BeautifulSoup
 import re
 from collections import Counter
+import streamlit as st
+import pandas as pd
 
-url = input("Enter the URL: ")
-html_content = requests.get(url).text
-soup = BeautifulSoup(html_content, "html.parser")
-all_text = soup.get_text()
-text = [
-    chunk.strip().lower()
-    for chunk in re.split(
-        r'[^a-zA-Z]+', all_text
-    )
-    if chunk.strip()
-]
-term_counts = Counter(text)
-ranked_terms = term_counts.most_common()
-
-print("\nRanked terms by frequency (most common first):")
-for rank, (term, count) in enumerate(ranked_terms, 1):
-    print(f"{rank}. {term}: {count}")
+def scraper(url):
+    html_content = requests.get(url).text
+    soup = BeautifulSoup(html_content, "html.parser")
+    all_text = soup.get_text()
+    text = [
+        chunk.strip().lower()
+        for chunk in re.split(
+            r'[^a-zA-Z]+', all_text
+        )
+        if chunk.strip()
+    ]
+    term_counts = Counter(text)
+    return term_counts.most_common()
+    
+st.write("Here's our first attempt at using data to create a table:")
+st.text_input("Website", key="site")
+if st.button("Get rankings!"):
+    if st.session_state.site.startswith("http://") or st.session_state.site.startswith("https://"):
+        url = st.session_state.site
+    else:
+        url = "http://" + st.session_state.site
+    st.write(pd.DataFrame(scraper(url)))
