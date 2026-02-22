24507ca2525e855f119bb89433ec43c4ece4d86a
Will Wagner <wowagner2015@yahoo.com>
2026-02-21 21:39:48 -0800

    put two pages in one

--- main.py
+++ main.py
@@ -12,6 +12,8 @@ import requests
 from bs4 import BeautifulSoup
 import re
 import io
+import requests
+from collections import Counter
 
 load_dotenv()
 
@@ -432,3 +434,31 @@ if do_find:
             st.info("No lists extracted. (Check if the topic yields relevant sources!)")
 else:
     st.info("Enter a topic and click **Find and Aggregate Top Lists** to start.")
+
+def scraper(url):
+    html_content = requests.get(url).text
+    soup = BeautifulSoup(html_content, "html.parser")
+    all_text = soup.get_text()
+    text = [
+        chunk.strip().lower()
+        for chunk in re.split(
+            r'(?<=[a-z])(?=[A-Z])|[^a-zA-Z\']+', all_text
+        )
+        if chunk.strip()
+    ]
+    term_counts = Counter(text)
+    
+    return term_counts.most_common()
+
+st.write('''
+---
+Zipf's Law:''')
+st.text_input("Website", key="site")
+if st.button("Get rankings!"):
+    if st.session_state.site.startswith("http://") or st.session_state.site.startswith("https://"):
+        url = st.session_state.site
+    else:
+        url = "http://" + st.session_state.site
+    df = pd.DataFrame(scraper(url), columns=["term", "count"])
+    df = df.sort_values(by="count", ascending=False)
+    st.bar_chart(df, x="term", y="count", stack=False, horizontal=True, sort=False)
